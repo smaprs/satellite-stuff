3f4bd532fff2221389c067051d97d5158f049f5d
smaprs <svolk2@hotmail.com>
2021-10-12 09:24:59 -0300

    Add files via upload

--- Sentinel-BASIC-SCRIPT-MONO.js
+++ Sentinel-BASIC-SCRIPT-MONO.js
@@ -1,13 +1,17 @@
-// BASIC MONO https://custom-scripts.sentinel-hub.com/sentinel-2/selective_enhancement_based_on_indices/ 
-function a(a, b) {return a + b}; function stretch(val, min, max) {return (val - min) / (max - min);} 
-function satEnh_V(rgbArr) {var avg = rgbArr.reduce((a, b) => a + b, 0) / rgbArr.length; return rgbArr.map(a => avg * (1 - StV) + a * StV);}
-function applyEnh_V(bArr) {return satEnh_V([stretch(bArr[0], SmV, SMV), stretch(bArr[1], SmV, SMV), stretch(bArr[2], SmV, SMV)]);}
-var S2_NDWI2 = (B03-B08)/(B03+B08);
-// WATER Turbidity // 
-var S2SN1=[B04*3+B11*.5,B03*1+B08*3,B02*3];//enh:.2;.8;2;
-// GEOLOGY // 
-var S2GF1=[B12*1.55+B04*.25,B05*1.2+B08*0.6,B02*2.8];// S2 L1C // enh:.1;.9;2;
-var MkV = S2GF1; 
-var SmV=0.1;var SMV=.9;var StV=2;
-var EnhV = applyEnh_V(MkV); 
-return EnhV 
+// MONO https://custom-scripts.sentinel-hub.com/sentinel-2/selective_enhancement_based_on_indices/ 
+function a(a, b) {return a + b;} 
+function stretch (val,min,max) {return (val-min)/(max-min);} //=ANNA
+function SATUREnh (rgbArr) {var avg = rgbArr.reduce((a, b) => a + b, 0) / rgbArr.length; 
+  return rgbArr.map(a => avg * (1 - SATUR) + a * SATUR);}
+function ApplyEnh (bArr) {return SATUREnh ([
+stretch (bArr[0], Rmin, Rmax), 
+stretch (bArr[1], Rmin, Rmax), 
+stretch (bArr[2], Rmin, Rmax)]);   }
+
+var S2Fire=[B04*1+B12*1,B03*1.6+B11*0.2+B08*0.3,B02*2.5];
+//var L8Fire=[B04*1.4+B07*1.5,B03*0.5+B05*0.2+B06*2,B02*3];
+
+var COMPO = S2Fire; 
+var Rmin=-0.1;var Rmax=0.9;var SATUR=2;
+
+var Enh = ApplyEnh (COMPO); return Enh;
